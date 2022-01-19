ca155c4be25b9051a4bce9601075117b122681ba
MichellDib <michell.dib28@gmail.com>
2022-01-19 11:05:46 +0100

    commit

--- task_1/lib/task1.ex
+++ task_1/lib/task1.ex
@@ -1,8 +1,12 @@
-# Simple Hello World print.
 defmodule Task1 do
+  # ---------------------------------------------------------------
+
+  # Simple Hello World print.
   def hello do
     :world
   end
+
+  # ---------------------------------------------------------------
 
   # Prints if inputted value is odd or even.
   def is_even(n) do
@@ -15,13 +19,38 @@ defmodule Task1 do
     end
   end
 
-  # Sums all the values in an array recursively.
+  # ---------------------------------------------------------------
+
+  # Base case that handles when array has been iterated through
   def sum([], sum) do
     sum
   end
-  def sum(list,sum) do
+
+  # Main recursion loop, sums all values in an inputted array.
+  def sum(list, sum) do
     [first_number | remaining_numbers] = list
     new_sum = sum + first_number
     sum(remaining_numbers, new_sum)
   end
+
+  # ---------------------------------------------------------------
+
+  # Handles special case where the entire list has been iterated through
+  def largest([] largest_value) do
+   largest_value
+  end
+
+  # Main recursion loop, compares head with the current largest value
+  def largest(list, largest_value) do
+    [head | tail] = list
+    if head > largest_value do
+      largest(tail, head)
+    else
+      largest(tail largest_value)
+    end
+  end
+
+  # ---------------------------------------------------------------
+
+
 end
