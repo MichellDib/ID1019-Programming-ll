e36da033be9e9c7f98b289eb3290f5bf05b5f9e8
MichellDib <michell.dib28@gmail.com>
2022-01-31 22:33:29 +0100

    commit

--- task_3/lib/task3.ex
+++ task_3/lib/task3.ex
@@ -1,70 +1,227 @@
-def defmodule Emulator do
+defmodule Task3 do
+  def test() do
+    {code, data} = Program.load(demo())
 
+    out = Out.start()
 
-  def run(prgm) do
-    {code, data} = Program.load(prgm)
-    reg = Registers.new()
-    run(0, code, reg, data)
+    reg = Register.new()
+
+    Emulator.run({code, data}, reg, out)
   end
 
+   def demo() do
+    {
+      {
+        {:addi, 1, 1, 5},
+        {:add, 4, 2, 1},
+        {:addi, 5, 0, 1},
+        {:label, :loop},
+        {:addi, 6, 0, 2},
+        {:addi, 5, 5, 1},
+        {:out, 5},
+        {:out, 6},
+        {:lw, 2, 0, 8},
+        {:sw, 2, 0, 8},
+        {:beq, 5, 6, :loop},
+        {:halt}
+      },
+      Tree.tree_new()
+    }
+  end
+end
 
-  def run(pc, code, reg, mem) do
+
+defmodule Out do
+  def start() do [] end
+
+  def close(out) do Enum.reverse(out) end
+
+  def put(out, s) do [s | out] end
+end
+
+
+
+
+defmodule Program do
+
+  def load(prgm) do
+    {code, data} = prgm
+  end
+
+  def read_instruction(code, pc) do
+    0 = rem(pc, 4)
+    elem(code, div(pc, 4))
+  end
+
+  def read_word(data, i) do
+    0 = rem(i, 4)
+  end
+
+  def write_word(data, i, val) do
+    0 = rem(i, 4)
+    Tree.tree_insert(i, val, data)
+  end
+end
+
+
+
+
+defmodule Register do
+  def new() do
+    reg =
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0}
+  end
+
+  def read(_, 0) do
+    0
+  end
+
+  def read(reg, i) do
+    IO.write("read from index: #{i}\n")
+    elem(reg, i)
+  end
+
+  def write(reg, 0, _) do
+    reg
+  end
+
+  def write(reg, i, val) do
+    put_elem(reg, i, val)
+  end
+end
+
+
+
+
+
+defmodule Tree do
+  def tree_new() do
+    {:node, nil, nil}
+  end
+
+  def tree_insert(index, insert_val, nil) do
+    {:leaf, index, insert_val}
+  end
+
+  def tree_insert(index, insert_val, {:leaf, i, val}) do
+    cond do
+      i == index -> {:leaf, index, insert_val}
+      i > index -> {:node, i, val, nil, {:leaf, insert_val}}
+      i < index -> {:node, i, val, {:leaf, insert_val}, nil}
+    end
+  end
+
+  def tree_insert(index, insert_val, {:node, nil, nil}) do
+    {:node, index, insert_val, nil, nil}
+  end
+
+  def tree_insert(index, insert_val, {:node, i, val, left, right}) do
+    cond do
+      index == i -> {:node, i, insert_val, left, right}
+      index > i -> {:node, i, val, left, tree_insert(index, insert_val, right)}
+      index < i -> {:node, i, val, tree_insert(index, insert_val, left), right}
+    end
+  end
+end
+
+
+
+
+
+defmodule Emulator do
+  def run(prgm, reg, out) do
+    {code, data} = Program.load(prgm)
+    run(0, code, reg, data, out)
+  end
+  def run(pc, code, reg, mem, out) do
     next = Program.read_instruction(code, pc)
+
     case next do
-      :halt ->
-        :ok
+      {:halt} ->
+        out = Out.close(out)
+        {reg, mem, out}
+
+        {:out, rs} ->
+          s = Register.read(reg, rs)
+          out = Out.put(out, s)
+
+          pc = pc + 4
+          run(pc, code, reg, mem, out)
+
       {:add, rd, rs, rt} ->
         pc = pc + 4
         s = Register.read(reg, rs)
         t = Register.read(reg, rt)
-        reg = Register.write(reg, rd, s + t) # well, almost
-        run(pc, code, reg, mem)
-        {:sub, rd, rs, rt} ->
-          pc = pc + 4
-          s = Register.read(reg, rs)
-          t = Register.read(reg, rt)
-          reg = Register.write(reg, rd, s - t) # well, almost
-          run(pc, code, reg, mem)
-        {:addi, rd, rt,imm} ->
-          pc = pc + 4
-          t = Register.read(reg, rt)
-          reg = Register.write(reg, rd, imm + t) # well, almost
-          run(pc, code, reg, mem)
-        {:lw, rd, rt,offset} ->
-          pc = pc + 4
-          t = Register.read(reg, offset+rt)
-          reg = Register.write(reg, rd, t) # well, almost
-          run(pc, code, reg, mem)
-        {:sw,rs ,rt,offset} ->
-          pc = pc + 4
-          s = Register.read(reg, rs)
-          reg = Register.write(reg, rt+offset, s) # well, almost
-          run(pc, code, reg, mem)
-        {:beq,rs ,rt,offset} ->
-          s = Register.read(reg, rs)
-          t = Register.read(reg, rt)
-          if(s==t)->
-            pc = pc + offset
-
-          else{
-            pc = pc + 4
-          }
-          run(pc, code, reg, mem)
-    end
-  end
-
-
-    def run(pc, code, reg, mem, out) do
-      next = Program.read_instruction(code, pc)
-      case next do
-        :halt ->
-          Out.close(out)
-      {:out, rs} ->
-        pc = pc + 4
-        s = Register.read(reg, rs)
-        out = Out.put(out, s)
+        reg = Register.write(reg, rd, s + t)
         run(pc, code, reg, mem, out)
 
-      end
+      {:sub, rd, rs, rt} ->
+        pc = pc + 4
+        s = Register.read(reg, rs)
+        t = Register.read(reg, rt)
+        reg = Register.write(reg, rd, s - t)
+        run(pc, code, reg, mem, out)
+
+      {:addi, rd, rt, imm} ->
+        pc = pc + 4
+        t = Register.read(reg, rt)
+        reg = Register.write(reg, rd, t + imm)
+        IO.write("code1: #{elem(reg,rd)}\n")
+        run(pc, code, reg, mem, out)
+
+      {:lw, rd, rt, offset} ->
+        pc = pc + 4
+        t = Program.read_word(mem, offset + rt)
+        reg = Register.write(reg, rd, t)
+        run(pc, code, reg, mem, out)
+
+      {:sw, rs, rt, offset} ->
+        pc = pc + 4
+        IO.write("#{rs}\n")
+        s = Register.read(reg, rs)
+        mem = Program.write_word(mem, rt + offset, s)
+        run(pc, code, reg, mem, out)
+
+      {:beq, rs, rt, offset} ->
+        s = Register.read(reg, rs)
+        t = Register.read(reg, rt)
+        if s == t do
+          pc = offset
+          out = Out.put(out, "Branch to = #{pc + offset}\n")
+          run(pc, code, reg, mem, out)
+        else
+          pc = pc + 4
+          out = Out.put(out, "\n")
+          run(pc, code, reg, mem, out)
+        end
+
+      {:label, label} ->
+        out = Out.put(out, "#{label}")
+        code = replace_labels(code, pc, pc, label)
+        pc = pc + 4
+        run(pc, code, reg, mem, out)
     end
   end
+
+  def replace_labels(code, start_pc, pc, label) do
+    next = Program.read_instruction(code, start_pc)
+
+    case next do
+      {:halt} ->
+        code
+
+      {:beq, s, t, l} ->
+        if l == label do
+
+          code = put_elem(code, div(start_pc, 4), {:beq, s, t, pc + 4})
+          replace_labels(code, start_pc + 4, pc, label)
+        else
+          replace_labels(code, start_pc + 4, pc, label)
+        end
+
+      _ ->
+        replace_labels(code, start_pc + 4, pc, label)
+    end
+  end
+end
